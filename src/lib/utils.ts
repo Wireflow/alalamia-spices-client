26ac5acda1e97ca02feae47150cdcde3db663f7a
kuroafa <kuroafa21@gmail.com>
2024-03-23 16:10:51 -0400

    .

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const dateFormatter = new Intl.DateTimeFormat('en-US', {   year: 'numeric',   month: 'long',   day: 'numeric', });
