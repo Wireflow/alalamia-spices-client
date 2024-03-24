8f3cc2f34149c4a7f84a6ec44d313382f099fac3
Nader Abdulrub <nabdulrub16@gmail.com>
2024-03-24 12:26:06 -0400

    fix auth hook

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -19,9 +19,9 @@ const setToken = (token: Token) => {
   Cookies.set(TOKEN_KEY, token, { secure: true, sameSite: "Strict" });
 };
 
-export const getToken = (): Token => {
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0aW1lIjoiU2F0IE1hciAyMyAyMDI0IDIxOjQ1OjI3IEdNVCswMDAwIChDb29yZGluYXRlZCBVbml2ZXJzYWwgVGltZSkiLCJpZCI6ImNsdTF2dTllMDAwMDIybzhhYjc1cnpiajQiLCJpYXQiOjE3MTEyMzAzMjcsImV4cCI6MTcxMTI4NjMyN30.z5l3zSf_-q8hogWnkKNYWogGkGDuhKaMveQ2Z1wJKP4" || Cookies.get(TOKEN_KEY);
-  return token || "";
+export const getToken = (): Token | null => {
+  const token = Cookies.get(TOKEN_KEY) || null;
+  return token;
 };
 
 const setSession = (session: Session) => {
